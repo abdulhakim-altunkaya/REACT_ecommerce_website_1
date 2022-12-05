10ecb01480ffafea225c73ced618ed09373aff96
abdulhakim_altunkaya <altunkaya2@gmail.com>
2022-12-05 21:22:00 +0300

    basket system has further improved, but still listing all elements and deleting them individiaully is left

--- src/components/LowBasket.js
+++ src/components/LowBasket.js
@@ -1,26 +1,58 @@
-import React from 'react'
+import React from 'react';
+import { useState } from 'react';
+import { useEffect } from 'react';
 
 function LowBasket() {
 
+  let [inputValue, setInputValue] = useState(1);
+  
+
   let basket = { ...localStorage };
-  let basket2 = JSON.parse(basket[2]);
-  console.log(basket);
-  console.log(basket2);
+  let basket2 = JSON.parse(basket[1]);
+
 
   for (let i = 0; i < localStorage.length; i++) {
-    let key2 = localStorage.key(i)
-    console.log(`${key2}: ${localStorage.getItem(key2)}`)
+    let key = localStorage.key(i);
+    let item = localStorage.getItem(key);
+    console.log(JSON.parse(item));
   }
 
+
+  let [sumValue, setSumValue] = useState();
+  useEffect(() => {
+    setSumValue(inputValue * basket2.price);
+  }, [inputValue]);
+  
   return (
     <div className='cartArea'>
       <h1>Prekių krepšelis</h1>
+
+      <div className='cartListHeader'>
+        <span>&nbsp;</span>
+        <span>Item Id</span>
+        <span>Title</span>
+        <span style={{paddingLeft: "10px"}}>Price</span>
+        <span>Unit</span>
+        <span>Sum</span>
+        <span>&nbsp;</span>
+      </div>
+
       <div className='cartList'>
 
         <div className='cartImageArea'>
             <img src={require('./Images/' + basket2.image +'.jpg')} alt={basket2.title} className="cartItemImage"/>
         </div>
-        <span>{basket2.title}</span>
+        <span>{basket2.number}</span>
+        <span className='cartTitleArea'>{basket2.title}</span>
+        <span style={{paddingLeft: "10px"}}>{basket2.price} Euro</span>
+        <div className='cartInputDiv'> 
+          <input type="number" value={inputValue} onChange={e => setInputValue(e.target.value)} className="cartInput" />
+        </div>
+        <span>{sumValue} Euro</span>
+        <div className='cartImageArea'>
+            <img src={require('./Images/' + "delete3.png")} alt="delete icon button" className="cartDeleteIcon"/>
+        </div>
+
 
       </div>
       
