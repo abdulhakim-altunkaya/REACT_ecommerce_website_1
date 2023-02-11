612a9ffcc52c66dffbc1d83817baaa1e4c52446b
abdulhakim_altunkaya <altunkaya2@gmail.com>
2023-02-11 13:41:55 +0300

    mapping function finished at the end, but it was really deep advanced level. I had to place two maps inside another another with json methods of javascript.

--- src/components/LowAdmin.js
+++ src/components/LowAdmin.js
@@ -1,36 +1,69 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import {db} from "../firebase";
-import { uid } from 'uid';
 import { ref, onValue} from "firebase/database";
+
 
 
 function LowAdmin() {
 
     const[orderArray, setOrderArray] = useState([]);
+   
 
-    useEffect(() => {
-      onValue(ref(db), snapshot => {
-        const data = snapshot.val();
-        if(data !== null) {
-            Object.values(data).map((order) => {
-                setOrderArray((oldArray) => [...oldArray, order]);
-            });
+
+    const handleClick = async () => {
+        //If not this if statement, each button click will add the same results under the existing ones
+        if(orderArray.length > 0) {
+            setOrderArray([]);
         }
-        console.log(orderArray[0]);
-        console.log(data);
-        console.log(JSON.parse(orderArray[0]));
-      });
-    }, [])
-    
+        onValue(ref(db), snapshot => {
+            let data = snapshot.val();
+            Object.values(data).map( (order) => {
+                setOrderArray( (oldArray) => [...oldArray, order] );
+            } );
+        });
 
 
+
+    }
 
     return (
         <div>
+            
+            <button onClick={handleClick} className="detailButton">DISPLAY ORDERS</button>
 
-                <div> 
-                    <span></span>
-                </div>
+
+            <div>
+                {/*If you have an object array and want to display it in React.js, then you need to use map.
+                Object array: [{}, {}, ....] */}
+                {orderArray.map((order, index) => (
+                    <div key={index} className="adminOrderMain">
+                        <div className='adminOrders'>
+                            <span><strong>Name: </strong> {order.FullName}</span>
+                            <span><strong>Address: </strong>  {order.Address}</span>
+                            <span><strong>E-mail: </strong>  {order.Email}</span>
+                            <span><strong>Telephone: </strong>  {order.Telephone}</span>
+                            <span><strong>Date: </strong>  {order.Date}</span>  
+                        </div>
+                        <div className='adminOrders'>
+                            <span>{JSON.parse(JSON.stringify(order.Order)).map( (item, index) => (
+                                <ul key={index} style={{"marginTop": 0}}> 
+                                    <li>
+                                        <strong>Item: </strong> {JSON.parse(item).title}, <br />
+                                        <strong>Price: </strong> {JSON.parse(item).price}, <br />
+                                        <strong>Unit: </strong> {JSON.parse(item).unit}, <br />
+                                        <strong>Item ID: </strong> {JSON.parse(item).number}, <br />
+                                    
+                                    </li> 
+                                </ul>
+                            ))}</span>
+
+
+
+                        </div>
+                    </div>
+                ))}
+                
+            </div>
 
         </div>
     )
